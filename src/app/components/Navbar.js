f1e9430b65667862541f08f5a6e53e9aef5e9e65
amadrafi <ahmadrafi0814@gmail.com>
2024-02-21 21:24:58 +0000

    update color and fonts

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -5,12 +5,12 @@ import AddCircleOutlineRoundedIcon from '@mui/icons-material/AddCircleOutlineRou
 
 function Navbar() {
   return (
-    <div className=" w-44 flex flex-initial min-h-lvh bg-zinc-900 content-center">
-      <div className="w-full h-56 self-center">
-        <ul className="text-right text-sm px-2 py-6 font-medium text-lg antialiased">
-          <li className="py-1 hover:bg-zinc-800 rounded-lg px-4"><Link href='/'>Overview <BarChartRoundedIcon /></Link></li>
-          <li className="py-1 hover:bg-zinc-800 rounded-lg px-4"><Link href='/classes'>Classes <ClassRoundedIcon /></Link></li>
-          <li className="py-1 hover:bg-zinc-800 rounded-lg px-4"><Link href='/add_lessons'>Add Lesson <AddCircleOutlineRoundedIcon /></Link></li>
+    <div className=" w-48 flex flex-initial min-h-lvh bg-zinc-900 content-center">
+      <div className="w-full h-56 self-center ">
+        <ul className="text-right font-poppins text-sm px-2 py-auto font-medium text-lg antialiased">
+          <li className="py-4 hover:bg-zinc-800 rounded-lg pl-4 pr-2"><Link href='/'>Overview <BarChartRoundedIcon /></Link></li>
+          <li className="py-4 hover:bg-zinc-800 rounded-lg pl-4 pr-2"><Link href='/classes'>Classes <ClassRoundedIcon /></Link></li>
+          <li className="py-4 hover:bg-zinc-800 rounded-lg pl-4 pr-2"><Link href='/add_lessons'>Add Lesson <AddCircleOutlineRoundedIcon /></Link></li>
         </ul>
       </div>
     </div>
--- src/app/page.js
+++ src/app/page.js
@@ -7,9 +7,9 @@ import UnderstandingGraph from "./components/UnderstandingGraph";
 
 export default function Home() {
   return (
-    <div className=" bg-zinc-800 text-white h-full max-w-full flex flex-col pt-3 p-2">
+    <div className="bg-slate-100 text-white h-full max-w-full flex flex-col pt-3 p-2">
       <div className="flex flex-row h-3/6">
-        <div className="w-3/5 flex">
+        <div className="w-3/5 flex pb-2">
           <LessonCarousels />
         </div>
         <div className="w-2/5 flex items-start pb-2">
@@ -17,11 +17,11 @@ export default function Home() {
         </div>
       </div>
       <div className="flex-row flex flex-auto">
-        <div className="w-1/3 px-4  py-2 flex items-center">
+        <div className="w-1/3 p-2 flex items-center">
           <ClassSummaries />
         </div>
         <div className="w-1/3 flex items-center">
-          <h1 className='mx-auto text-center'>
+          <h1 className='mx-auto text-center text-black'>
             FREE SPACE!!!
           </h1>
           {/* <UploadPicture /> */}
