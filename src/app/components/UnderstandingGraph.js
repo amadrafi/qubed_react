c446d7af19672be4ca4cbba07b206c86053fe1d5
amadrafi <ahmadrafi0814@gmail.com>
2024-03-29 21:13:32 +0000

    final touches

--- src/app/components/UnderstandingGraph.js
+++ src/app/components/UnderstandingGraph.js
@@ -20,7 +20,7 @@ function UnderstandingGraph(){
       <ResponsiveContainer width="100%" height="90%" className='px-3'>
         <LineChart data={data} margin={{ top: 10, right: 10, left: -30, bottom: 20 }}>
           <XAxis dataKey="name" stroke="grey"/>
-          <YAxis domain={["dataMin", "dataMax"]} padding={{top: 20, bottom: 100}}/>
+          <YAxis domain={["dataMin", "dataMax"]} padding={{top: 60, bottom: 100}}/>
           <Tooltip/>
           <Line type="monotone" dataKey="understanding" stroke="#8884d8" strokeWidth={2} activeDot={{ r: 6 }}/>
           <Line type="monotone" dataKey="engagement" stroke='#8f4215' strokeWidth={2} activeDot={{ r: 6 }}/>
